2748f69f2673471f2d63013687e0df202544829a
pablofdez-git <pablo.fernandezcornejo@gmail.com>
2025-04-29 09:19:43 +0200

    Correccion Ejercicio 5

--- Ejercicio6_5.sql
+++ Ejercicio6_5.sql
@@ -1,18 +1,19 @@
 --6.5 Realizar un procedimiento PL/SQL trabajado sobre las tablas PRODUCTOS, FABRICANTES que muestre por
 --pantalla el producto más caro de todos los que contiene la tabla y el fabricante al que corresponde.
 
+
+
 SET SERVEROUTPUT ON
 
-CREATE OR REPLACE PROCEDURE producto_mas_caro IS
-  v_precio_maximo productos.precio%TYPE;
-  v_nombre_producto productos.nombre%TYPE;
-  v_nombre_fabricante fabricantes.nombre%TYPE;
+DECLARE
+  v_nombre_producto VARCHAR2(100);
+  v_precio NUMBER(6,2);
+  v_nombre_fabricante VARCHAR2(100);
 BEGIN
-    SELECT MAX(precio), nombre, f.nombre
-    INTO v_precio_maximo, v_nombre_producto, v_nombre_fabricante
-    FROM productos p
-    JOIN fabricantes f
-    ON p.id_fabricante = f.id;
-    
-    DBMS_OUTPUT.PUT_LINE('El producto más caro es ' || v_nombre_producto || ' y pertenece al fabricante ' || v_nombre_fabricante);
+  SELECT p.nombre, p.precio, f.nombre
+  INTO v_nombre_producto, v_precio, v_nombre_fabricante
+  FROM productos, p fabricantes f WHERE p.id_fabricante = f.id
+  AND p.precio = (SELECT MAX(precio) FROM productos);
+  
+  DBMS_OUTPUT.PUT_LINE('El producto más caro es: ' || v_nombre_producto || ' y su fabricante es: ' || v_nombre_fabricante);
 END;
